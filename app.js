b1349ec7081485b47d91dffcea2ffead88ea5081
Nikita <lavro.nikita123@gmail.com>
2023-02-21 12:39:27 +0200

    API: Tours GET request handler

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+//Core
+const fs = require('fs');
+
 const express = require('express');
 const app = express();
 
-const port = 3000;
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+const port = 80;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
