44c86e8e512d7da1120d09fe893cd98071c8d0fc
Nikita <lavro.nikita123@gmail.com>
2023-02-21 16:29:57 +0200

    Add test middlewares to the request-response cycle

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const app = express();
 //Middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 //Files
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -15,8 +25,10 @@ const tours = JSON.parse(
 
 //Functions
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -95,7 +107,7 @@ const deleteTour = (req, res) => {
 };
 
 //Router
-app.route('api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
