fde59b3355dc6659f83fc7c6e91300693292b3c5
Nikita <lavro.nikita123@gmail.com>
2023-02-21 13:19:33 +0200

    API: tours POST request handler

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //Core
 const fs = require('fs');
 
+//Express
 const express = require('express');
 const app = express();
 
+//Middleware
+app.use(express.json());
+
+//Files
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -16,6 +21,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.at(-1).id + 1;
+  const newTour = { ...req.body, id: newId };
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 80;
