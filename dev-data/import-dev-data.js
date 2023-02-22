fa7b408ca674bbd29312916e066fd0ea5ed527b3
Nikita <lavro.nikita123@gmail.com>
2023-02-22 15:28:27 +0200

    read JSON file and import data into DB function

--- dev-data/import-dev-data.js
+++ dev-data/import-dev-data.js
@@ -19,3 +19,16 @@ mongoose
   .then(() => {
     console.log('DB conection successful');
   });
+
+//READ JSON FILE
+const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+
+//IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded');
+  } catch (error) {
+    console.log();
+  }
+};
