8518ef9f6ff73915662a92e6f9d237579501fb09
Nikita <lavro.nikita123@gmail.com>
2023-02-22 15:12:08 +0200

    tourModel: imageCover, image, createdAt, startDates

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,6 +41,16 @@ const tourSchema = new mongoose.Schema({
     type: String,
     trim: true,
   },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  image: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
