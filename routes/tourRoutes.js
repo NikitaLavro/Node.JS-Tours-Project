dfc0786ec42043dd3d9bb1ae7a563c8d388686c7
Nikita <lavro.nikita123@gmail.com>
2023-02-23 16:20:36 +0200

    getTourStats aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,11 +9,13 @@ const {
   updateTour,
   deleteTour,
   aliasTopCheapTours,
+  getTourStats,
 } = require('../controllers/tourController');
 
 //router.param('id', checkId);
 
 //Router
+router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-cheap').get(aliasTopCheapTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
