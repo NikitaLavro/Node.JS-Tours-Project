f168cf31561c31c3ba2683d0ccfbc6d52acd6f42
Nikita <lavro.nikita123@gmail.com>
2023-02-23 14:16:12 +0200

    exporting functionality into an apiFeatures class

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,9 @@
 //Models
 const Tour = require('../models/tourModel');
 
+//Utils
+const APIFeatures = require('../utils/apiFeatures');
+
 //Middlewares
 exports.aliasTopCheapTours = (req, res, next) => {
   req.query.limit = '5';
@@ -9,52 +12,15 @@ exports.aliasTopCheapTours = (req, res, next) => {
   next();
 };
 
-//Functions
 exports.getAllTours = async (req, res) => {
   try {
-    //Build query
-    //1) Filtering
-    const queryObj = { ...req.query };
-    const excludedFIelds = ['page', 'sort', 'limit', 'fields'];
-    excludedFIelds.forEach((el) => delete queryObj[el]);
-
-    //Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    const advFilteredResultsObj = JSON.parse(queryStr);
-
-    let query = Tour.find(advFilteredResultsObj);
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    //3) Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query.select('-__v');
-    }
-
-    //4 Pagination
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     //Execute query
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     //Send Response
     res.status(200).json({
@@ -67,7 +33,7 @@ exports.getAllTours = async (req, res) => {
   } catch (error) {
     res.status(404).json({
       status: 'fail',
-      message: error,
+      message: error.message,
     });
   }
 };
