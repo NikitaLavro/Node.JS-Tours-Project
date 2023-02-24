b9ea06dc67dbe03b3549f09a414c756ad80f5b0d
Nikita <lavro.nikita123@gmail.com>
2023-02-24 20:39:23 +0200

    unhandled rejection error catching

--- server.js
+++ server.js
@@ -21,6 +21,14 @@ mongoose
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection, closing down the server...');
+  server.close(() => {
+    process.exit();
+  });
 });
