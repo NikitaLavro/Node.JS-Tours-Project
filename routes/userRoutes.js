9518985e4f49f64b81fad5c89c34b5168d9b612d
Nikita <lavro.nikita123@gmail.com>
2023-02-25 12:52:04 +0200

    creating a signup route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,7 +10,10 @@ const {
   deleteUser,
 } = require('../controllers/userController');
 
+const { signup } = require('../controllers/authController');
+
 //Router
+router.post('/signup', signup);
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
