51fde207b55333c5a78bd0842e114c0ee54d4324
Nikita <lavro.nikita123@gmail.com>
2023-02-25 17:30:50 +0200

    getAllUsers implementation and login check if user exists initial logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,10 +29,13 @@ exports.login = catchAsync(async (req, res, next) => {
 
   //1) Check if email and password exist
   if (!email || !password) {
-    next(new AppError('Please provide email and password', 400));
+    return next(new AppError('Please provide email and password', 400));
   }
 
   //2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  console.log(user);
 
   //3) If everything ok, send the token to the client
   const token = 'testToken';
