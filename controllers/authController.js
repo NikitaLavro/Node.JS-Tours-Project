03f38b8142d3757170eeb4c3ae9e3e6699bd86f6
Nikita <lavro.nikita123@gmail.com>
2023-02-25 16:53:07 +0200

    authController initial login implementation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const appError = require('../utils/appError');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -20,5 +22,23 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //1) Check if email and password exist
+  if (!email || !password) {
+    next(new AppError('Please provide email and password', 400));
+  }
+
+  //2) Check if user exists && password is correct
+
+  //3) If everything ok, send the token to the client
+  const token = 'testToken';
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
