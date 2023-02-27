66228960ed5812fd78113cc0c87ca0250cac004a
Nikita <lavro.nikita123@gmail.com>
2023-02-27 17:59:16 +0200

    authController restrict to function

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,7 +13,7 @@ const {
   getMonthlyPlan,
 } = require('../controllers/tourController');
 
-const { protect } = require('../controllers/authController');
+const { protect, restrictTo } = require('../controllers/authController');
 
 //router.param('id', checkId);
 
@@ -22,7 +22,10 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-cheap').get(aliasTopCheapTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour);
-//.delete(protect, restrictTo('admin'), deleteTour);
+router
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router;
