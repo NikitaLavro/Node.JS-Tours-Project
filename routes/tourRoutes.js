390dcebc5ea121a9e0be5daa29a49d060b1ea433
Nikita <lavro.nikita123@gmail.com>
2023-02-27 17:46:50 +0200

    userModel role field

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -22,6 +22,7 @@ router.route('/monthly-plan/:year').get(getMonthlyPlan);
 router.route('/tour-stats').get(getTourStats);
 router.route('/top-5-cheap').get(aliasTopCheapTours, getAllTours);
 router.route('/').get(protect, getAllTours).post(createTour);
-router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+router.route('/:id').get(getTour).patch(updateTour);
+//.delete(protect, restrictTo('admin'), deleteTour);
 
 module.exports = router;
