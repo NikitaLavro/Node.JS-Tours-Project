f91c01e7e1b4ddf5e0c07e086fd9ac2e99ce3cfe
Nikita <lavro.nikita123@gmail.com>
2023-02-28 18:05:15 +0200

    fix user variable bug by awaiting the query

--- controllers/authController.js
+++ controllers/authController.js
@@ -180,7 +180,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
   //1) Get user from the collection
-  const user = User.findById(req.user.id).select('+password');
+  const user = await User.findById(req.user.id).select('+password');
 
   //2) Check if POSTed password is correct
   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
