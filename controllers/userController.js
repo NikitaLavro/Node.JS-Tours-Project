453a243f106fb42688681c0d20207ea743fb0c92
Nikita <lavro.nikita123@gmail.com>
2023-03-01 11:18:47 +0200

    updateMe: create error

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
@@ -12,6 +13,19 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
       users,
     },
   });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  //2) Update user document
 });
 
 exports.getUser = (req, res) => {
