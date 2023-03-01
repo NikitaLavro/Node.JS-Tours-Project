c89ac5ccac838425b65785e55b7598da09863d56
Nikita <lavro.nikita123@gmail.com>
2023-03-01 11:54:01 +0200

    filterObject function implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,14 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
