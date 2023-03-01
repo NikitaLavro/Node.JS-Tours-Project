48f6a36c86e746ff14224158c5b32d26fc8d41bb
Nikita <lavro.nikita123@gmail.com>
2023-03-01 16:15:03 +0200

    Data sanitization against NoSQL query injection

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 //APP IMPORTS
 const AppError = require('./utils/appError');
@@ -37,6 +39,11 @@ app.use(express.json({ limit: '10kb' }));
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XXS attacks
 
 //Test middleware
 app.use((req, res, next) => {
