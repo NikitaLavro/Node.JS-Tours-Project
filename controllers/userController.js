bb4a48ea0f56d10ff0f0b741ad9d2f14cf55d3fc
Nikita <lavro.nikita123@gmail.com>
2023-03-01 11:41:39 +0200

    updateMe: filterBody and updatedUser initial implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,6 +26,12 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     );
   }
   //2) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
   res.status(200).json({
     status: 'success',
   });
