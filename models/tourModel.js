36c89d7d8e0fd1344cf45b5925d93f793a82e3a4
Nikita <lavro.nikita123@gmail.com>
2023-03-06 21:31:17 +0200

    tourSchema Review virtual populate

--- models/tourModel.js
+++ models/tourModel.js
@@ -113,6 +113,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return (this.duration / 7).toFixed(2);
 });
 
+//VIRTUAL POPULATE
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 //DOCUMENT MIDDLEWARE : runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
