7e604cda73ce42980bf17b35534b01ffb6c39a5f
Nikita <lavro.nikita123@gmail.com>
2023-03-06 15:11:39 +0200

    reviewRoutes complete routing

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const reviewController = require('../controllers/reviewController');
+
+const {
+  getAllReviews,
+  createReview,
+} = require('../controllers/reviewController');
+
+const { protect, restrictTo } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.route('/');
+router
+  .route('/')
+  .get(getAllReviews)
+  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router;
