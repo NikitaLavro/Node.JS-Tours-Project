643696164175b23847c0bead1940e258ac714da0
Nikita <lavro.nikita123@gmail.com>
2023-03-07 21:00:21 +0200

    tourController: factory deleteOne

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -14,6 +14,9 @@ exports.aliasTopCheapTours = (req, res, next) => {
   next();
 };
 
+//Factory
+const factory = require('./handlerFactory');
+
 exports.getAllTours = catchAsync(async (req, res, next) => {
   //Execute query
   const features = new APIFeatures(Tour.find(), req.query)
@@ -77,18 +80,7 @@ exports.updateTour = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-
-  if (!tour) {
-    return next(new AppError('No Tour Found with that ID', 404));
-  }
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-});
+exports.deleteTour = factory.deleteOne(Tour);
 
 exports.getTourStats = catchAsync(async (req, res, next) => {
   const stats = await Tour.aggregate([
