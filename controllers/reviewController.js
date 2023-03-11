0c79c79cc757726cd32276bf53a5772ff26ddd8d
Nikita <lavro.nikita123@gmail.com>
2023-03-11 14:00:56 +0200

    reviewController: getAllReviews factory function

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,26 +4,13 @@ const catchAsync = require('../utils/catchAsync');
 //Factory
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
-
 exports.setTourUserIds = (req, res, next) => {
   if (!req.body.tour) req.body.tour = req.params.tourId;
   if (!req.body.user) req.body.user = req.user.id;
   next();
 };
+
+exports.getAllReviews = factory.getAll(Review);
 
 exports.getReview = factory.getOne(Review);
 
