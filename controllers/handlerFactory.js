40fbc89cd32c6a681970b77d48da9e5184dfef76
Nikita <lavro.nikita123@gmail.com>
2023-03-11 13:57:33 +0200

    getAll factory function: allow for nested GET reviews on tour

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -66,8 +66,12 @@ exports.getOne = (Model, populateOptions) =>
 
 exports.getAll = (Model) =>
   catchAsync(async (req, res, next) => {
+    //To allow for nested GET reviews on tour
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
     //Execute query
-    const features = new APIFeatures(Model.find(), req.query)
+    const features = new APIFeatures(Model.find(filter), req.query)
       .filter()
       .sort()
       .limitFields()
