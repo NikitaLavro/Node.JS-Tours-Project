be5d4fca524805983f61d51b325052e7ac98c568
Nikita <lavro.nikita123@gmail.com>
2023-03-14 00:00:09 +0200

    reviewModel: calcAverageRating static function

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -37,6 +37,20 @@ reviewSchema.pre(/^find/, function (next) {
   next();
 });
 
+reviewSchema.static.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    { $match: { tour: tourId } },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(stats);
+};
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
