b5ef6202e490f74f9c306b2f28c5e2b15f0129c2
Nikita <lavro.nikita123@gmail.com>
2023-03-22 00:33:43 +0200

    basic pug template

--- app.js
+++ app.js
@@ -77,7 +77,10 @@ app.use(globalErrorHandler);
 
 //APP ROUTER
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
