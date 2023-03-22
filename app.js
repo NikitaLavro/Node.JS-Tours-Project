b4d45a18995b241fdc5d1cd7a4b22ca720287744
Nikita <lavro.nikita123@gmail.com>
2023-03-23 00:55:30 +0200

    set up project structure

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 //MIDDLEWARES
 
@@ -76,25 +77,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(globalErrorHandler);
 
 //APP ROUTER
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
 
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours',
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
---
+++ routes/viewRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { getOverview, getTour } = require('../controllers/viewsController');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
+router.get('/', getOverview);
+router.get('/tour', getTour);
+
+module.exports = router;
