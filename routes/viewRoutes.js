ec179f7186f76cfd502a543059ca6a4f24818410
Nikita <lavro.nikita123@gmail.com>
2023-03-24 01:40:11 +0200

    getTour route and initial function

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,14 +3,7 @@ const { getOverview, getTour } = require('../controllers/viewsController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas',
-  });
-});
-
-router.get('/dick', getOverview);
-router.get('/tour', getTour);
+router.get('/', getOverview);
+router.get('/tour/:slug', getTour);
 
 module.exports = router;
