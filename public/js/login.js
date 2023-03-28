18e4b034d97c8d448a6991fee236a5d9d6828c4c
Nikita <lavro.nikita123@gmail.com>
2023-03-29 01:28:13 +0300

    login: sending a POST request

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,20 @@
 /* eslint-disable */
 
-const login = (email, password) => {
-  alert(email, password);
+const login = async (email, password) => {
+  console.log(email, password);
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://127.0.0.1:80/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+    console.log(res);
+  } catch (error) {
+    console.log(error.response.data);
+  }
 };
 
 document.querySelector('.form').addEventListener('submit', (e) => {
