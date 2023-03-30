50d13433e80db61796fc7fd22cb9478c32debd35
Nikita <lavro.nikita123@gmail.com>
2023-03-31 01:39:39 +0300

    refactoring index.js

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,21 @@ import '@babel/polyfill';
 import { displayMap } from './mapbox';
 import { login } from './login';
 
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+//DOM ELEMENTS
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
 
-displayMap(locations);
+//DELEGATION
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
 
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  login(email, password);
-});
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
