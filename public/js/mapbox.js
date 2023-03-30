4c0ec049b881507faa32d7fe65876b7d15f71110
Nikita <lavro.nikita123@gmail.com>
2023-03-31 01:23:04 +0300

    configuring files for parcel

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,50 +1,47 @@
 /* eslint-disable */
 
-console.log('hello from the client');
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
+export const displayMap = (locations) => {
+  mapboxgl.accessToken =
+    'pk.eyJ1Ijoic291bHNoYXJkIiwiYSI6ImNsZnJicGc4ZDA0NmUzcnFmM2lsbnY5ajcifQ.D4X9luNDj4qV9XnZX3Ek5A';
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/soulshard/clfrcf9th011501pe6krhexfc', // style URL
+    scrollZoom: false,
+  });
 
-console.log(locations);
+  const bounds = new mapboxgl.LngLatBounds();
 
-mapboxgl.accessToken =
-  'pk.eyJ1Ijoic291bHNoYXJkIiwiYSI6ImNsZnJicGc4ZDA0NmUzcnFmM2lsbnY5ajcifQ.D4X9luNDj4qV9XnZX3Ek5A';
-const map = new mapboxgl.Map({
-  container: 'map', // container ID
-  style: 'mapbox://styles/soulshard/clfrcf9th011501pe6krhexfc', // style URL
-  scrollZoom: false,
-});
+  locations.forEach((loc) => {
+    //Crate marker
+    const el = document.createElement('div');
+    el.className = 'marker';
 
-const bounds = new mapboxgl.LngLatBounds();
+    //Add marker
+    new mapboxgl.Marker({
+      element: el,
+      anchor: 'bottom',
+    })
+      .setLngLat(loc.coordinates)
+      .addTo(map);
 
-locations.forEach((loc) => {
-  //Crate marker
-  const el = document.createElement('div');
-  el.className = 'marker';
+    //Add popup
+    new mapboxgl.Popup({
+      offset: 30,
+    })
+      .setLngLat(loc.coordinates)
+      .setHTML(`<p>${loc.day}:${loc.description}</p>`)
+      .addTo(map);
 
-  //Add marker
-  new mapboxgl.Marker({
-    element: el,
-    anchor: 'bottom',
-  })
-    .setLngLat(loc.coordinates)
-    .addTo(map);
+    // Extend map bounds to include current location
+    bounds.extend(loc.coordinates);
+  });
 
-  //Add popup
-  new mapboxgl.Popup({
-    offset: 30,
-  })
-    .setLngLat(loc.coordinates)
-    .setHTML(`<p>${loc.day}:${loc.description}</p>`)
-    .addTo(map);
-
-  // Extend map bounds to include current location
-  bounds.extend(loc.coordinates);
-});
-
-map.fitBounds(bounds, {
-  padding: {
-    top: 200,
-    bottom: 150,
-    left: 100,
-    right: 100,
-  },
-});
+  map.fitBounds(bounds, {
+    padding: {
+      top: 200,
+      bottom: 150,
+      left: 100,
+      right: 100,
+    },
+  });
+};
