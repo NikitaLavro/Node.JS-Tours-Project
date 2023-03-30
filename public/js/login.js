4695a35bb3fa346779a89dea4ea7246abf87cf85
Nikita <lavro.nikita123@gmail.com>
2023-03-31 02:37:33 +0300

    frontend logout function

--- public/js/login.js
+++ public/js/login.js
@@ -25,3 +25,15 @@ export const login = async (email, password) => {
     console.log(error);
   }
 };
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://127.0.0.1:80/api/v1/users/logout',
+    });
+    if ((res.data.status = 'success')) location.reload(true);
+  } catch (error) {
+    showAlert('error', 'Error logging out! Try agaon');
+  }
+};
