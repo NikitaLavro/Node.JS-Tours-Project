d1723b28ced44d902815bc834ea494ec00d52295
Nikita <lavro.nikita123@gmail.com>
2023-04-11 21:16:54 +0300

    sending password reset, sendgrid emails + minor bug fixes

--- utils/email.js
+++ utils/email.js
@@ -12,8 +12,14 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      //Sendgrid
-      return 1;
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
 
     return nodemailer.createTransport({
@@ -26,10 +32,10 @@ module.exports = class Email {
     });
   }
 
-  //Send the actual email
+  // Send the actual email
   async send(template, subject) {
     // 1) Render HTML based on a pug template
-    const html = pug.renderFile(`${__dirname}/..views/emails/${template}.pug`, {
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
       firstName: this.firstName,
       url: this.url,
       subject,
@@ -40,14 +46,22 @@ module.exports = class Email {
       from: this.from,
       to: this.to,
       subject,
-      text: htmlToText.fromString(html),
       html,
+      //text: htmlToText.fromString(html),
     };
+
     // 3) Create a transport and send email
     await this.newTransport().sendMail(mailOptions);
   }
 
   async sendWelcome() {
-    await this.send('welcome', 'Welcome to the Natours family');
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
   }
 };
