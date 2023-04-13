4f088e00aa1ffde34109d35130d16024486f347b
Nikita <lavro.nikita123@gmail.com>
2023-04-14 01:01:49 +0300

    delete console.logs

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -72,7 +72,6 @@ reviewSchema.post('save', function () {
 
 reviewSchema.pre(/^findOneAnd/, async function (next) {
   this.r = await this.findOne();
-  console.log(this.r);
 });
 
 reviewSchema.post(/^findOneAnd/, async function () {
