1a8b5a9cfb4407d131505aecce093d934ef21a7e
Nikita <lavro.nikita123@gmail.com>
2023-04-15 01:22:22 +0300

    line items image fix

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -31,7 +31,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`http://127.0.0.1/img/tours/${tour.imageCover}`],
+            images: [
+              `${req.protocol}://${req.get('host')}/img/tours/${
+                tour.imageCover
+              }`,
+            ],
           },
           unit_amount: tour.price * 100,
         },
